01cde3e82d202dde64e0e5b01f779b10bb1dea66
Tyler Marenger <46637887+tjmareng@users.noreply.github.com>
2019-02-08 11:11:01 -0500

    Added pause menu script
    This script allows the pause menu scene to pop up when the 'ESC' key is pressed (while the game is running). This script contains the 'resume' and 'pause' method features. When the game is paused the game time will stop/freeze. The console will be messaged when the game is paused or resumed.

---
+++ PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool IsGamePaused = false;
+    public GameObject pauseMenuUI;
+
+	// Update is called once per frame
+	void Update () {
+
+        // if 'ESC' key is pressed while running
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // if the game is paused
+            if (IsGamePaused)
+            {
+                Resume(); // call resume function
+            }
+            // otherwise
+            else
+            {
+                Pause(); // call pause function
+            }
+        }
+	}
+
+    /*
+     * This method deactiveates the pause menu, resumes the game, and returns the message 'Resume' to console.
+     */
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false); // deactivate gameObject PauseMenu
+        Time.timeScale = 1f; // start game time
+        IsGamePaused = false; // cahnge boolean to false
+        Debug.Log("Resume"); // message console
+    }
+
+    /*
+     * This method activates the pause menu, stops the game, and returns the message 'Pause' to console.
+     */
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true); // activate gameObject PauseMenu
+        Time.timeScale = 0f; // stop game time
+        IsGamePaused = true; // change boolean to true
+        Debug.Log("Pause"); // message console
+    }
+}
