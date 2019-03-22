b4d1052b5fb80b41d1977223fcbebe1583b101e4
Colin Arkens <42915107+cjarkens@users.noreply.github.com>
2019-03-22 18:09:50 -0400

    Added EventTrigger

---
+++ LostInMazieMansion/Assets/Scripts/EventTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTrigger : MonoBehaviour
+{
+    bool triggered = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (triggered == false)
+        {
+            Debug.Log("Event Triggered");
+            triggered = true;
+        }
+
+    }
+}
