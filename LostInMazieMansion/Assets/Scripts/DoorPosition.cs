990002bbf01727d6585023e0f1948ab147082dbf
Crystal Massoglia <42586306+gossamac@users.noreply.github.com>
2019-03-24 11:21:41 -0400

    Scene Flippers - see longer description
    Note:  Not entirely finished, but functional
    DoorManager - ScriptableObject, defines which room the door goes to and the position of the door
    DoorPosition - helps define the position of the door, used in RoomManager to make things easier
    RoomManager - flips the scenes when a door is walked through

---
+++ LostInMazieMansion/Assets/Scripts/DoorPosition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorPosition : MonoBehaviour
+{
+    public DoorManager door;
+
+    private Transform doorPos;
+
+    private void Start()
+    {
+        doorPos = GetComponent<Transform>();
+        doorPos.transform.position = new Vector3(door.x, door.y, door.z);
+    }
+}
