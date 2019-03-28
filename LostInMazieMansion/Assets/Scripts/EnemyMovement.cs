498e55a1cca0ba9ba546862fe0993161e799fcf7
Tyler Marenger <tjmareng@mtu.edu>
2019-03-28 15:29:32 -0400

    Documented EnemyMovement Script

--- LostInMazieMansion/Assets/Scripts/EnemyMovement.cs
+++ LostInMazieMansion/Assets/Scripts/EnemyMovement.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
 
-    public float speed;
-    private Transform target;
+    public float speed; // Enemy Movement Speed
+    private Transform target; // Player
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +17,10 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // If the distance between the target(player) and the enemy is less than 3,
         if(Vector2.Distance(transform.position, target.position) < 3)
         {
+            // Then the enemy will follow the target(player) at the given speed
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
         }
         
