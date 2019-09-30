4807a2dd9ebc14b89872cea68e363ee213760423
Christian LaCourt <cllacour@mtu.edu>
2019-09-30 19:38:55 -0400

    A bit more

---
+++ LostInMazieMansion/Assets/Scripts/MaziesMansion/UI/InventoryItemDisplayer.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MaziesMansion
+{
+    [RequireComponent(typeof(Image))]
+    internal sealed class InventoryItemDisplayer : MonoBehaviour
+    {
+
+        //TODO
+    }
+}
---
+++ LostInMazieMansion/Assets/Scripts/MaziesMansion/UI/PopulateInventoryItems.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace MaziesMansion
+{
+    internal sealed class PopulateInventoryItems : MonoBehaviour
+    {
+        public InventoryItemDisplayer DisplayerPrefab;
+
+        public void Populate()
+        {
+
+        }
+    }
+}
