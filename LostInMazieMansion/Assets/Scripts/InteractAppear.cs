d529665fc362b698b6d17add3383fc00ffcf464e
Tyler Marenger <tjmareng@mtu.edu>
2019-10-14 19:17:31 -0400

    feat(interaction): created script for image to appear on interact

---
+++ LostInMazieMansion/Assets/Scripts/InteractAppear.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractAppear : MonoBehaviour
+{
+    [SerializeField] private Image EtoInteractImage;
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        if (otherCollider.CompareTag("Player"))
+        {
+            Debug.Log("E to Interact");
+
+            EtoInteractImage.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D otherCollider)
+    {
+        if (otherCollider.CompareTag("Player"))
+        {
+            Debug.Log("Disappear");
+
+            EtoInteractImage.enabled = false;
+        }
+    }
+}
