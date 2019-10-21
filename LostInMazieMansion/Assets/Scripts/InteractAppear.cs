1e4f98cd3fb7df6ecbb102e22f97d62517271f47
Tyler Marenger <tjmareng@mtu.edu>
2019-10-21 17:54:31 -0400

    feat(interactions): update position of e to interact on game objects

--- LostInMazieMansion/Assets/Scripts/InteractAppear.cs
+++ LostInMazieMansion/Assets/Scripts/InteractAppear.cs
@@ -6,15 +6,19 @@ using UnityEngine.UI;
 public class InteractAppear : MonoBehaviour
 {
     [SerializeField] private Image EtoInteractImage;
+    private bool stillTouchingTrigger = false;
 
     private void OnTriggerEnter2D(Collider2D otherCollider)
     {
-        if (otherCollider.CompareTag("Player"))
-        {
-            Debug.Log("E to Interact");
-
-            EtoInteractImage.enabled = true;
-        }
+      
+            if (otherCollider.CompareTag("Player"))
+            {
+                Debug.Log("E to Interact");
+                stillTouchingTrigger = true;
+                
+            }
+        
+       
     }
 
     private void OnTriggerExit2D(Collider2D otherCollider)
@@ -22,8 +26,17 @@ public class InteractAppear : MonoBehaviour
         if (otherCollider.CompareTag("Player"))
         {
             Debug.Log("Disappear");
-
+            stillTouchingTrigger = false;
             EtoInteractImage.enabled = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (stillTouchingTrigger)
+        { 
+            EtoInteractImage.transform.position = this.gameObject.transform.position;
+            EtoInteractImage.enabled = true;
         }
     }
 }
