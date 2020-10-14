fa098faf7cf07a2d90a6220faac728141303756a
Tyler Marenger <tjmareng@mtu.edu>
2020-10-14 15:42:06 -0400

    feat(Entryway): Grand Entryway fully functional from F1

--- LostInMazieMansion/Assets/Scripts/MaziesMansion/Doors/Stairway.cs
+++ LostInMazieMansion/Assets/Scripts/MaziesMansion/Doors/Stairway.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class Stairway : MonoBehaviour
 {
-    bool toggle;
+    enum EntrywayLocation
+    {
+        F1,
+        F2
+    }
+
+    EntrywayLocation activeFloor;
     GameObject Floor1;
     GameObject Floor2;
 
@@ -14,22 +20,21 @@ public class Stairway : MonoBehaviour
         // Get Floor 1 and Floor 2 grids
         Floor1 = GameObject.Find("F1").transform.Find("Grid").gameObject;
         Floor2 = GameObject.Find("F2").transform.Find("Grid").gameObject;
-    }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        Floor1.SetActive(false);
-        Floor2.SetActive(true);
+        activeFloor = EntrywayLocation.F1; // TO DO: UPDATE TO CHECK ON ENTRY
+
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        
-    }
+        // Find exit direction
+        Vector3 direction = other.transform.position - transform.position;
 
-    // Update is called once per frame
-    void Update()
-    {
-
+        // Set corresponding active grids
+        Floor1.SetActive((direction.y > 0) ? false : true);
+        Floor2.SetActive((direction.y < 0) ? false : true);
+        activeFloor = (direction.y > 0) ? EntrywayLocation.F2 : EntrywayLocation.F1;
     }
 }
+
+
