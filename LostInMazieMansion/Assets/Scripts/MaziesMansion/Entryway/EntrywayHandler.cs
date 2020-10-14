b2e6f3698188dc8880080897daa26497537c3e8e
Tyler Marenger <tjmareng@mtu.edu>
2020-10-14 16:00:34 -0400

    feat(Entryway): connected F2 doorways

---
+++ LostInMazieMansion/Assets/Scripts/MaziesMansion/Entryway/EntrywayHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntrywayHandler : MonoBehaviour
+{
+    GameObject Floor1;
+    GameObject Floor2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Check what floor the player is on
+
+        // Get Floor 1 and Floor 2 grids
+        Floor1 = GameObject.Find("F1").transform.Find("Grid").gameObject;
+        Floor2 = GameObject.Find("F2").transform.Find("Grid").gameObject;
+
+
+        // Set correct floor grid active
+
+
+    }
+}
